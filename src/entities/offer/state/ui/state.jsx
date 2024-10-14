5f23f0d8090375163ebc704793a2da9c728699a1
VitalyDu <viduwebjob@gmail.com>
2024-10-14 04:43:40 +0300

    fix: offers state

--- src/entities/offer/state/ui/state.jsx
+++ src/entities/offer/state/ui/state.jsx
@@ -1,17 +1,14 @@
-import { useInitData, useLaunchParams } from "@telegram-apps/sdk-react";
 import { Caption } from "@telegram-apps/telegram-ui";
+import { cx } from "class-variance-authority";
 import { useTranslation } from "react-i18next";
 import styles from "./state.module.css";
-import { cx } from "class-variance-authority";
 
-export const OfferState = ({ state = "inWork" }) => {
-  const initDataRaw = useLaunchParams().initDataRaw;
-  const initData = useInitData();
+export const OfferState = ({ state = "in_work" }) => {
   const { t } = useTranslation();
 
   return (
     <div className={cx(styles.state, styles[`${state}`])}>
-      <Caption>{state}</Caption>
+      <Caption>{t(`statuses.${state}`)}</Caption>
     </div>
   );
 };
