df23c6de9fff2d607750fb4dc78363fa92ff2daa
VitalyDu <viduwebjob@gmail.com>
2024-10-26 20:01:43 +0300

    add: toastify

--- src/main.jsx
+++ src/main.jsx
@@ -4,5 +4,6 @@ import "./index.css";
 import "./mockEnv.js";
 import "@telegram-apps/telegram-ui/dist/styles.css";
 import "./i18n";
+import "react-toastify/dist/ReactToastify.css";
 
 createRoot(document.getElementById("root")).render(<Root />);
