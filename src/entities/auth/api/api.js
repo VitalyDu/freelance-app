a1f9272367cea5966323f253db0c59306b6e5e1a
VitalyDu <viduwebjob@gmail.com>
2024-10-27 01:49:37 +0300

    add: signin, getMe

--- src/entities/auth/api/api.js
+++ src/entities/auth/api/api.js
@@ -1,17 +1,8 @@
-// import { post } from "@/shared/api/baseApi";
-
-// export const authApi = {
-//   checkTelegram: ({ data }) => {
-//     post({ url: `/check_telegram`, data: data });
-//   },
-//   signUp: ({ data }) => {
-//     post({ url: `/register`, data: data });
-//   },
-// };
-
 import { baseApi } from "@/shared/api";
 
 export const authApi = {
   checkTelegram: ({ data }) => baseApi.post("/check_telegram", data),
   signUp: (pin, data) => baseApi.post(`/register?pin_code=${pin}`, data),
+  signIn: (pin, data) => baseApi.post(`/login?pin_code=${pin}`, data),
+  getMe: () => baseApi.get(`/me`),
 };
--- src/entities/auth/model/store.js
+++ src/entities/auth/model/store.js
@@ -6,7 +6,7 @@ export class AuthStore {
   abortController = null;
   data = {};
   accessToken = "";
-  registered = undefined; // Поле для хранения токена
+  registered = undefined;
 
   constructor() {
     makeAutoObservable(this, {}, { autoBind: true });
@@ -16,12 +16,13 @@ export class AuthStore {
     this.loading = true;
     this.abortController = null;
     this.data = {};
-    this.accessToken = ""; // Очистка токена
+    this.accessToken = "";
   };
 
   checkTelegram = async (initDataRow) => {
+    this.loading = true;
+
     try {
-      this.loading = true;
       const res = await authApi.checkTelegram({
         data: { request_str: initDataRow },
       });
@@ -37,12 +38,46 @@ export class AuthStore {
   };
 
   signUp = async (pin, initDataRow) => {
+    this.loading = true;
+
     try {
-      this.loading = true;
       const res = await authApi.signUp(pin, { request_str: initDataRow });
       if (res.status === 200 && res.data) {
         runInAction(() => {
-          // this.registered = res.data?.registered;
+          this.accessToken = res.data?.access_token;
+          this.registered = true;
+          this.loading = false;
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  signIn = async (pin, initDataRow) => {
+    this.loading = true;
+
+    try {
+      const res = await authApi.signIn(pin, { request_str: initDataRow });
+      if (res.status === 200 && res.data) {
+        runInAction(() => {
+          this.accessToken = res.data?.access_token;
+          this.loading = false;
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  getMe = async () => {
+    this.loading = true;
+
+    try {
+      const res = await authApi.getMe();
+      if (res.status === 200 && res.data) {
+        runInAction(() => {
+          this.data = res.data;
           this.loading = false;
         });
       }
